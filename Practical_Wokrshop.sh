6a1c25415e28e9259c5a111b68a65ba394085c3c
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-03-23 13:16:11 +0000

    Encrypted mail

--- Practical_Wokrshop.sh
+++ Practical_Wokrshop.sh
@@ -1,4 +1,6 @@
-uname -a
+uname -a //Shows system info
 which gpg
-gpg --version
-gpg --full-generate-key
+gpg --version //Shows GPG version info
+gpg --armor --export 
+gpg --list-secret-keys --keid-format=long //Shows the lists of the private keys
+gpg --armor --export-secret-keys XXXXXX // Exports the secret keys
