bd5b028e93b07ffecaf6ff90517eab5fc524d472
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-03-23 13:29:33 +0000

    Recibimiento del mensaje y desencriptacion

--- Practical_Wokrshop.sh
+++ Practical_Wokrshop.sh
@@ -4,3 +4,4 @@ gpg --version //Shows GPG version info
 gpg --armor --export 
 gpg --list-secret-keys --keid-format=long //Shows the lists of the private keys
 gpg --armor --export-secret-keys XXXXXX // Exports the secret keys
+gpg --
