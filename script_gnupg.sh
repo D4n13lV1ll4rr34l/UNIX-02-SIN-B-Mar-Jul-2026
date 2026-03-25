2225db1dac5a43c28e67295fa551202155bb4a05
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-03-25 16:28:24 +0000

    Verify partner binary signed document

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -8,3 +8,4 @@ gpg --encrypt --sign --armor -r davidsitotorres8@gmail.com doc_no_cifrado.txt
 gpg --edit-key davidsitotorres8@gmail.com # trust level 4 (full) - David is my classmate
 gpg --sign-key davidsitotorres8@gmail.com
 gpg --verify Daniel_doc_no_cifrado_firmado.txt
+gpg --verify Daniel_doc_no_cifrado_firmado_binario.txt
