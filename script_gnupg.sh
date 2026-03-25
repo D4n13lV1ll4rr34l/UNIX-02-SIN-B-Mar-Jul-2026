64e79ecbf1d824cac961c3999a529d46b4addc48
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-03-25 16:09:09 +0000

    Encrypt and sign file simultaneously

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -4,3 +4,4 @@ gpg --list-keys
 gpg --clearsign doc_no_cifrado.txt
 gpg --sign doc_no_cifrado.txt
 gpg --detach-sign doc_no_cifrado.txt
+gpg --encrypt --sign --armor -r davidsitotorres8@gmail.com doc_no_cifrado.txt
