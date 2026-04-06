1b13ba6cec1178f14556045b53dba1beef303d36
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-06 12:42:14 +0000

    Explanation of the commands and the outputs

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+
+
+"ls -lai" This command is used to list files and directories, but with more detailed information.
+Output: "total 12
+925742 drwxr-xr-x 2 root root 4096 Apr  6 12:34 .
+918514 drwxr-xr-x 1 root root 4096 Apr  6 12:34 .." 
+
+"sudo mkdir proyecto"" "total 12 : Dis command create a new folder name proyecto using administration power
