56178dd0d4727d9956265c3d148e4d1d1fc4e0b6
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-06 12:17:17 +0000

    clean repository

--- Practical_Wokrshop.sh
+++
@@ -1,7 +0,0 @@
-uname -a //Shows system info
-which gpg
-gpg --version //Shows GPG version info
-gpg --armor --export 
-gpg --list-secret-keys --keid-format=long //Shows the lists of the private keys
-gpg --armor --export-secret-keys XXXXXX // Exports the secret keys
-gpg --
