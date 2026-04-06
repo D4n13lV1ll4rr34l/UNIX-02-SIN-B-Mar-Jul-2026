f4733917919fa3df38a52a9b1e16a423e5e076f8
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-06 12:57:45 +0000

    Description of the stats

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,4 @@
-
+"/home" Dis command is used to navigate to the home directory of the user 
 
 "ls -lai" Dis command is used to list files and directories, but with more detailed information.
 Output: "total 12
@@ -6,4 +6,6 @@ Output: "total 12
 918514 drwxr-xr-x 1 root root 4096 Apr  6 12:34 .." 
 
 "sudo mkdir proyecto"" "total 12 : Dis command create a new folder name proyecto using administration power
-"%stat"stat is used fi examine files deeply
+"%stat": For checking di current directory, status and permissions
+-p: create parent directories if they do not already exist
+/tmp/prueba/sub1/tmp/prueba/sub2: Dis is the full path of nested folders to be created
