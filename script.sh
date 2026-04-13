4e5897905e310215c40c66044a370b05fb449bee
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-13 12:57:30 +0000

    Descriptions of man ls

--- script.sh
+++ script.sh
@@ -5,3 +5,5 @@ ls -l: Shows detailed information about each file:
 ls -h: Show information about files human readable
 rm -rf: Deletes files or directories, Deletes directories and everything inside them, including all subfolders and files, ignores warnnings
 rm -- -rf: Treats files such as arguments, not options.
+ls --help: Shows the help page for the ls command, including all available options and how to use them.
+man ls: Opens the full manual page for the ls command.
