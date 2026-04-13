599b4e1375e2767e79483415656f68e68d14adf3
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-13 12:41:25 +0000

    List of commands such as -h

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 ls -a: Lists the contents of a directory (files and folders).
 ls -all: Shows all files, including hidden files.
 ls -a /: Lists directory contents, Includes hidden files (those starting with .) Refers to the root directory (the top-level directory in Linux)
+ls -l: Shows detailed information about each file:
+ls -h: Show information about files human readable
