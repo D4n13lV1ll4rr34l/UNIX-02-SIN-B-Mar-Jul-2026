2500a4662e7be3650d5c24b2d4c656834e6ed9ef
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-13 12:45:53 +0000

    Inclusion of rm -- rf

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@ ls -all: Shows all files, including hidden files.
 ls -a /: Lists directory contents, Includes hidden files (those starting with .) Refers to the root directory (the top-level directory in Linux)
 ls -l: Shows detailed information about each file:
 ls -h: Show information about files human readable
+rm -rf: Deletes files or directories, Deletes directories and everything inside them, including all subfolders and files, ignores warnnings
+rm -- -rf: Treats files such as arguments, not options.
