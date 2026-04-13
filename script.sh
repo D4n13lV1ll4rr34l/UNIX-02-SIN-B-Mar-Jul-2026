749e703811f5bf39a23581b804a328e104014b34
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-04-13 12:32:07 +0000

    List of -a /

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 ls -a: Lists the contents of a directory (files and folders).
 ls -all: Shows all files, including hidden files.
+ls -a /: Lists directory contents, Includes hidden files (those starting with .) Refers to the root directory (the top-level directory in Linux)
