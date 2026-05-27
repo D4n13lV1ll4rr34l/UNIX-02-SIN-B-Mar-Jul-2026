4f1a062a7770ed492df630aa64ee761880398508
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-05-27 13:01:44 +0000

    Showing the main groups of the user

---
+++ .devcontainer/scriptgroup.sh
@@ -0,0 +1,6 @@
+id #see the main actual group
+id -gn #Only showcase the name of the main group
+touch ~/test_grupo_heredado.txt #create an archive and see which group inherits
+ls -la ~/test_grupo_heredado.txt
+
+#the group is the main group of the user
