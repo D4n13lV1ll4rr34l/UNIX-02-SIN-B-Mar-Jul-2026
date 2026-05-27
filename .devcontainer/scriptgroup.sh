f78a1a670e3435849442f9f2e711856dec1c2718
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-05-27 13:07:20 +0000

    create an archive before newgrp

--- .devcontainer/scriptgroup.sh
+++ .devcontainer/scriptgroup.sh
@@ -4,3 +4,12 @@ touch ~/test_grupo_heredado.txt #create an archive and see which group inherits
 ls -la ~/test_grupo_heredado.txt
 
 #the group is the main group of the user
+
+#show the actual group
+id -gn
+echo "Grupo actual: $(id-gn)"
+
+#create an archive before newgrp
+
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
