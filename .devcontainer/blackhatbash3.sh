ab828bb028d5d05c56003ad6fefcacd4e8a988e8
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-06-01 13:40:42 +0000

    echo of root directory

--- .devcontainer/blackhatbash3.sh
+++ .devcontainer/blackhatbash3.sh
@@ -9,7 +9,11 @@ echo "This book's name is ${book}
 # Prints the string to the terminal, substituting the variable '$book' with its current value using a short-hand syntax.
 echo "This book's name is $book"
 
+# Executes 'ls -ld /' to get root directory details and stores the output string in 'root_directory'.
+root_directory=$(ls -ld /)
 
+# Prints the stored root directory metadata to the terminal using the variable's value.
+echo "${root_directory}"
 
 
 
