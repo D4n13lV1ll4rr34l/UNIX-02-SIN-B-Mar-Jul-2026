c07b1fe926dd833e47e735d7f846293bc20585be
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-06-01 13:39:02 +0000

    black hat turns book

--- .devcontainer/blackhatbash3.sh
+++ .devcontainer/blackhatbash3.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Assigns the string value 'black hat bash' to the variable named 'book'.
+book="black hat bash"
 
+# Prints the string to the terminal, substituting the variable '${book}' with its current value.
+echo "This book's name is ${book}
 
-
-
+# Prints the string to the terminal, substituting the variable '$book' with its current value using a short-hand syntax.
+echo "This book's name is $book"
 
 
 
