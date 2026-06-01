5f9d1368cbb9458f6f1ca1044238903ac50ed1b3
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-06-01 13:01:20 +0000

    Creation of blackhatbash

---
+++ .devcontainer/blackhatbash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+# UNIX COMMAND DOCUMENTATION
+# bash --version : Displays the installed GNU Bash shell version and build details.
+# env            : Lists all active global environment variables (e.g., SHELL, LANG, PWD).
+# echo ${RANDOM} : Returns a pseudo-random integer between 0 and 32767.
+# echo ${OSTYPE} : Identifies the current operating system environment (e.g., linux-gnu).
+# ps -e -f       : Lists all active processes (-e) with full details (-f) like PID and PPID.
+# ps -ef         : Identical to 'ps -e -f' (lists all active processes in full format).
+# df --human-readable : Shows disk space usage of all filesystems in readable units (GB, MB).
+# bash --version : Displays the installed GNU Bash shell version and build details.
