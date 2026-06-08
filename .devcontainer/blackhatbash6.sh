2e75d0378eb2e0a65628fb30c610aff87148ddbc
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-06-08 12:51:41 +0000

    pingin target

--- .devcontainer/blackhatbash6.sh
+++ .devcontainer/blackhatbash6.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
 # This script pings any address provided as an argument.
 
 SCRIPT_NAME="${0}"
@@ -8,4 +7,10 @@ TARGET="${1}"
 
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"
+
+# Display all arguments passed to the script as a single list
+echo "The arguments are: $@"
+
+# Display the total count of arguments passed to the script
+echo "The total number of arguments is: $#"
