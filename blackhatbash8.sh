c8b6487eb4067a37021ec1ff8809c165eda9661f
D4n13lV1ll4rr34l <143050972+D4n13lV1ll4rr34l@users.noreply.github.com>
2026-06-17 12:43:29 +0000

    Modification of blackhatbash8: head and logs

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -11,3 +11,13 @@ awk '{print $1,$2,$3}' log.txt
 # Extracts and displays the first and last fields from each line in log.txt.
 # $1 refers to the first field, while $NF refers to the last field in the record.
 awk '{print $1,$NF}' log.txt
+
+# Extract and display the first column from a comma-separated values (CSV) file.
+# The -F',' option sets the field delimiter to a comma, allowing awk to
+# correctly parse and process CSV-formatted records.
+awk -F',' '{print $1}' example_csv.txt
+
+# Display the first lines of the log file to perform a quick inspection
+# of the recorded HTTP requests, source IP addresses, timestamps,
+# requested resources, response codes, and user-agent information.
+head log.txt
